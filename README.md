b583936afac22a0e1dcfc5d77aaa6b475089362c
Rob Bygrave <robin.bygrave@gmail.com>
2022-09-22 15:01:04 +1200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # reentrant-vs-synchronized
+
+JMH Benchmark to compare `synchronized` and `ReentrantLock`
+
+#### Build
+```sh
+mvn clean package
+```
+
+#### 1 Thread, 2 forks
+```sh
+java -jar target/bench-jmh.jar -f 2 -t 1
+```
+
+#### 10 Threads, 2 forks
+```sh
+java -jar target/bench-jmh.jar -f 2 -t 10
+```
+
+#### Help, command line args
+```sh
+java -jar target/bench-jmh.jar -h
+```
---
+++ src/main/java/org/example/LockTest.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockTest {
+
+  private static Object lock = new Object();
+  private static ReentrantLock reentrantLock = new ReentrantLock();
+  private static long cnt = 0;
+
+  @Benchmark
+  @Measurement(iterations = 3)
+  @Threads(10)
+  @Fork(2)
+  @Warmup(iterations = 5, time = 10)
+  public long testWithoutLock(){
+    return doSomething();
+  }
+
+  @Benchmark
+  @Measurement(iterations = 3)
+  @Threads(10)
+  @Fork(2)
+  @Warmup(iterations = 5, time = 10)
+  public long testReentrantLock(){
+    reentrantLock.lock();
+    try {
+      return doSomething();
+    } finally {
+      reentrantLock.unlock();
+    }
+  }
+
+  @Benchmark
+  @Measurement(iterations = 3)
+  @Threads(10)
+  @Fork(2)
+  @Warmup(iterations = 5, time = 10)
+  public long testSynchronized(){
+    synchronized (lock) {
+      return doSomething();
+    }
+  }
+
+  private long doSomething() {
+    cnt += 1;
+    if (cnt == (Long.MAX_VALUE)) {
+      cnt = 0;
+    }
+    return cnt;
+  }
+
+//  public static void main(String[] args) {
+//    Options options = new OptionsBuilder().include(LockTest.class.getSimpleName()).build();
+//    try {
+//      new Runner(options).run();
+//    } catch (Exception e) {
+//
+//    } finally {
+//    }
+//  }
+}
